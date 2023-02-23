2238c645d66c45c90922ccd0ae232f1d7c5102da
Ryan Sun <ryan24.sun@gmail.com>
2023-02-22 22:30:46 -0500

    Form stuff is hard

--- app.js
+++ app.js
@@ -9,10 +9,24 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+//Website Pages
+app.get("/" || "/home", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/accomodations", function(req, res){
+    res.sendFile(__dirname + "/accomodations.html");
+});
+
+app.get("/about", function(req, res){
+    res.sendFile(__dirname + "/about.html");
+});
+
+app.get("/reserve" || "/book" || "/booknow", function(req, res){
+    res.sendFile(__dirname + "/reserve.html");
+});
+
+//Mailchimp API
 app.post("/", function(req, res){
 
     const email = req.body.email;
@@ -46,7 +60,20 @@ app.post("/", function(req, res){
 
 });
 
+app.post("/reserve", function(req, res){
+
+    const arrivalDate = req.body.arrivalDate;
+    const departureDate = req.body.departureDate;
+
+    const rooms = req.body.rooms;
+    // const adults = req.body.adults.value;
+    // const children = req.body.children.value;
+    console.log(arrivalDate + departureDate);
+    console.log(rooms);
+
+})
+
 //Allows testing for app on Heroku and local system
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Server is running on port 3000");
+    console.log("Server is running");
 });
